e6e867bdcc4d210fa2e5d4c4f32754a654073b5d
ural360 <75667422+ural360@users.noreply.github.com>
2024-05-18 21:39:16 +0500

    Add files via upload

--- proj/main.py
+++ proj/main.py
@@ -1,5 +1,5 @@
 #Импорт
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -15,7 +15,6 @@ def result_calculate(size, lights, device):
 @app.route('/')
 def index():
     return render_template('index.html')
-
 #Вторая страница
 @app.route('/<size>')
 def lights(size):
@@ -28,7 +27,7 @@ def lights(size):
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',
+                            'electronics.html',                           
                             size = size, 
                             lights = lights                           
                            )
@@ -42,4 +41,32 @@ def end(size, lights, device):
                                                     int(device)
                                                     )
                         )
+#Форма
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Результаты формы
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    #Создай переменные для сбора информации
+    name = request.form['name']
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+    with open('form.txt', 'a',) as f:
+            f.write(name,
+                    email,\
+                    address,\
+                    date) 
+
+    # здесь вы можете сохранить данные или отправить их по электронной почте
+    return render_template('form_result.html', 
+                           #Помести переменные
+                           name=name,
+                           email=email,
+                           address=address,
+                           date=date
+                           )
+
 app.run(debug=True)
